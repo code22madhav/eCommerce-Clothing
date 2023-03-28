7d5f13847508f12fa2c053fb6c391e06bee60f75
code22madhav <madhav22pathak@gmail.com>
2023-03-29 01:04:36 +0530

    initial setup react

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+const App= ()=> {
+  return (
+    <div clasName="categories-container">
+      <div clasName="category-container">
+      {/* <img src="" alt="" /> */}
+        <div className="category-body-container">
+            <h2>hats</h2>
+            <p>Shop Now</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
